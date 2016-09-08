4fff93bc874a9c8f68cbd6153256cc08da650895
Jonathan <jonacom@lissismore.com>
2016-09-08 12:13:09 -0400

    remove template-function.cpp

--- for_each.cpp
+++ for_each.cpp
@@ -2,16 +2,14 @@
 #include <algorithm>
 #include <vector>
 
-using namespace std;
-
 template <typename Collection, typename unop>
 void my_for_each(Collection col, unop op) {
-  for_each(col.begin(), col.end(), op);
+  std::for_each(col.begin(), col.end(), op);
 }
 
 int main() {
-  auto echo = [](int i) { cout << i << endl; };
-  vector<int> col{20, 30, 50, 10, 22, 44, 89, 19};
+  auto echo = [](int i) { std::cout << i << std::endl; };
+  std::vector<int> col{20, 30, 50, 10, 22, 44, 89, 19};
 
   my_for_each(col, echo);
   return 0;
