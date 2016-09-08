22f24da5bf553d991aa47918f99cb6198183e561
Jonathan <jonacom@lissismore.com>
2016-09-08 12:14:20 -0400

    update template function name

--- for_each.cpp
+++ for_each.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 template <typename Collection, typename unop>
-void my_for_each(Collection col, unop op) {
+void forEach(Collection col, unop op) {
   std::for_each(col.begin(), col.end(), op);
 }
 
@@ -11,6 +11,6 @@ int main() {
   auto echo = [](int i) { std::cout << i << std::endl; };
   std::vector<int> col{20, 30, 50, 10, 22, 44, 89, 19};
 
-  my_for_each(col, echo);
+  forEach(col, echo);
   return 0;
 }
